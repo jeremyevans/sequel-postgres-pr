52bf7614e6868780a93e0c8e3dc07ddbaf906820
Jeremy Evans <code@jeremyevans.net>
2010-06-15 13:52:30 -0700

    Make encode_bytea support ruby 1.9

--- lib/postgres-pr/typeconv/bytea.rb
+++ lib/postgres-pr/typeconv/bytea.rb
@@ -1,20 +1,17 @@
 module Postgres::Conversion
-
-  #
   # Encodes a string as bytea value.
   #
-  # for encoding rules see:
+  # For encoding rules see:
   #   http://www.postgresql.org/docs/7.4/static/datatype-binary.html
   #
-
   def encode_bytea(str)
-    str.gsub(/[\000-\037\047\134\177-\377]/) {|b| "\\#{ b[0].to_s(8).rjust(3, '0') }" }
+    # each_byte used instead of [] for 1.9 compatibility
+    str.gsub(/[\000-\037\047\134\177-\377]/){|b| "\\#{sprintf('%o', b.each_byte{|x| break x}).rjust(3, '0')}"}
   end
 
-  #
   # Decodes a bytea encoded string.
   #
-  # for decoding rules see:
+  # For decoding rules see:
   #   http://www.postgresql.org/docs/7.4/static/datatype-binary.html
   #
   def decode_bytea(str)
@@ -22,5 +19,4 @@ module Postgres::Conversion
       if s.size == 2 then s[1,1] else s[1,3].oct.chr end
     }
   end
-
 end
