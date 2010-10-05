4d941c6eef7c25c5459269265c13de2be7576a6d
Jeremy Evans <code@jeremyevans.net>
2010-10-05 14:45:16 -0700

    Add support for PostgreSQL 9.0 bytea hex format when decoding, bump version to 0.6.4
    Encoding is always done in the historical escape format, so that
    a version check is not needed and it works with all supported
    PostgreSQL versions.

--- lib/postgres-pr/typeconv/bytea.rb
+++ lib/postgres-pr/typeconv/bytea.rb
@@ -4,6 +4,7 @@ module Postgres::Conversion
   # For encoding rules see:
   #   http://www.postgresql.org/docs/7.4/static/datatype-binary.html
   #
+  # The historical bytea escape format is always used, for maximum compatibility.
   def encode_bytea(str)
     # each_byte used instead of [] for 1.9 compatibility
     str.gsub(/[\000-\037\047\134\177-\377]/){|b| "\\#{sprintf('%o', b.each_byte{|x| break x}).rjust(3, '0')}"}
@@ -14,9 +15,16 @@ module Postgres::Conversion
   # For decoding rules see:
   #   http://www.postgresql.org/docs/7.4/static/datatype-binary.html
   #
+  # Supports both the historical escape format and the new 9.0+ hex format.
   def decode_bytea(str)
-    str.gsub(/\\(\\|'|[0-3][0-7][0-7])/) {|s|
-      if s.size == 2 then s[1,1] else s[1,3].oct.chr end
-    }
+    if str =~ /\A\\x/
+      # PostgreSQL 9.0+ bytea hex format
+      str[2..-1].gsub(/(..)/){|s| s.to_i(16).chr}
+    else
+      # Historical PostgreSQL bytea escape format
+      str.gsub(/\\(\\|'|[0-3][0-7][0-7])/) {|s|
+        if s.size == 2 then s[1,1] else s[1,3].oct.chr end
+      }
+    end
   end
 end
